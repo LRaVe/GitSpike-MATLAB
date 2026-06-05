4bdee4a718ad406777cffc0e8cd44387a58157f8
Maximax-B <maxime.beltoise@ensea.fr>
2026-06-05 22:38:28 +0200

    Update generate_SP_dataset.m

--- ALGO_Simple/Gen_data/generate_SP_dataset.m
+++ ALGO_Simple/Gen_data/generate_SP_dataset.m
@@ -4,65 +4,60 @@
 
 
 function spikes = generate_SP_dataset(params)
-    
-    N = params.N;
-    c = params.c;
-    
-    S = params.S;
-    R = params.R;
-    
+
+    N    = params.N;
+    c    = params.c;
+
+    S    = params.S;
+    R    = params.R;
+
     Tmax = params.Tmax;
     rate = params.rate;
-    
+
     spikes = cell(N,S,R);
-    
-    %% =========================
-    %% STIMULI
-    %% =========================
-    
+
+    pooledRate = c * rate;
+
     for s = 1:S
-    
-        %% pooled train du stimulus
-    
-        nSpikes = poissrnd(rate*Tmax);
-    
+
+        %% train poolé du stimulus
+
+        nSpikes = poissrnd(pooledRate*Tmax);
+
         pooledTrain = sort(rand(1,nSpikes)*Tmax);
-    
-        %% repetitions
-    
+
         for r = 1:R
-    
-            %% reset coding neurons
-    
+
+            %% reset neurones codants
+
             for n = 1:c
                 spikes{n,s,r} = [];
             end
-    
-            %% -------------------------
-            %% coding neurons
-            %% -------------------------
-    
-            for k = 1:length(pooledTrain)
-    
-                neuron = randi(c);
-    
-                spikes{neuron,s,r} = [spikes{neuron,s,r} pooledTrain(k)];
-    
+
+            %% distribution aléatoire
+
+            assignment = randi(c,1,nSpikes);
+
+            for n = 1:c
+
+                spikes{n,s,r} = pooledTrain(assignment==n);
+
             end
-    
-            %% -------------------------
-            %% non-coding neurons
-            %% -------------------------
-    
+
+            %% neurones non codants
+
             for n = c+1:N
-    
+
                 nNoise = poissrnd(rate*Tmax);
-    
+
                 spikes{n,s,r} = sort(rand(1,nNoise)*Tmax);
-    
+
             end
+
         end
+
     end
+
 end
 
 
