c7e787c925f76e5f6c504d8ad21820ee6f8b6383
LaureWOLFF <laure.wolff@ensea.fr>
2026-08-04 11:36:55 +0200

    Supressing the folder algo_base

--- algo_base/MEX_version/calculate_integrated_P_optimized_mex_m.m
+++
@@ -1,7 +0,0 @@
-%% Script to compute the performance in summed population hypothesis 
-% Date: May-June 2026
-% Author : Laure WOLFF
-
-function [P, MatrixD] = calculate_integrated_P_optimized_mex_m(CellMatrix, selection, S, R, tmin, tmax, metric)
-    [P, MatrixD] = calculate_integrated_P_optimized_mex(CellMatrix, selection, S, R, tmin, tmax, metric);   
-end
